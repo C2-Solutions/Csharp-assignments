472b64145bce721d1b0763ef041aaf42116a3ad6
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-06-03 22:46:28 +0200

    PetShop: Implement Main, Animals

---
+++ PetShop/Animals.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace PetShop
+{
+    public abstract class Animal
+    {
+        public string sound { get; private protected set; }
+        public double price { get; private protected set; }
+        public double weight { get; private protected set; }
+
+        public virtual string GetSpecies() => GetType().Name;
+        public virtual void MakeSound() => Console.WriteLine(sound);
+    }
+
+    // Individual Animals
+    public class Dog : Animal
+    {
+        /// <summary>
+        /// Constructor for Dog. Sets defaults props.
+        /// </summary>
+        public Dog()
+        {
+            sound = "Woof woof";
+            price = 249.99;
+            weight = 15;
+        }
+    }
+
+    public class Bird : Animal
+    {
+        /// <summary>
+        /// Constructor for Bird. Sets defaults props.
+        /// </summary>
+        public Bird()
+        {
+            sound = "Tweet";
+            price = 49.99;
+            weight = 0.25;
+        }
+    }
+
+
+public class Fish : Animal
+    {
+        /// <summary>
+        /// Constructor for Fish. Sets defaults props.
+        /// </summary>
+        public Fish()
+        {
+            sound = "Blub blub";
+            price = 4.99;
+            weight = 0.1;
+        }
+    }
+
+    public class Blåhaj : Animal
+    {
+        /// <summary>
+        /// Constructor for Blåhaj. Sets defaults props.
+        /// Behold the Blåhaj: https://i.imgur.com/BXkQR2E.png https://i.imgur.com/ClqpU4f.mp4
+        /// </summary>
+        public Blåhaj()
+        {
+            sound = "Feed me the flesh of the fallen";
+            price = 30;
+            weight = 0.64;
+        }
+    }
+}
---
+++ PetShop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+#pragma warning disable CS8632
+namespace PetShop
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to our petshop!");
+
+            // Defining animals and sellables to loop over later.
+            List<Animal?> animals = new()
+            {
+                new Dog(),
+                new Bird(),
+                new Fish(),
+                // new Blåhaj()
+            };
+
+
+            // All the printing
+            Console.WriteLine("We have the following animals on display:");
+            PrintSpecies(animals);
+
+            Console.WriteLine("\nIf you listen closely, you can even hear them!");
+            PrintSounds(animals);
+
+            Console.WriteLine("\nWe also sell the following toys and treats:");
+        }
+
+        // Main functions
+        public static void PrintSpecies(List<Animal?> animals)
+        {
+            foreach (Animal animal in animals) {
+                if (animal is not null) Console.WriteLine($"- {animal.GetSpecies()}");
+            }
+        }
+
+        public static void PrintSounds(List<Animal?> animals)
+        // Prints the sound of every animal
+        {
+            foreach (Animal animal in animals) {  if (animal is not null) animal.MakeSound(); }
+        }
+
+        // Helper functions
+        public static string CurrencyToString(double value)
+        {
+            return value.ToString("C", CultureInfo.CurrentCulture);
+        }
+    }
+}
