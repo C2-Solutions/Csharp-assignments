f3841bc0bdefb58428782733fd076002dd10b22a
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-06-06 05:03:09 +0200

    PetShop: Add Sellables logic

--- PetShop/Program.cs
+++ PetShop/Program.cs
@@ -2,7 +2,6 @@
 using System.Globalization;
 using System.Collections.Generic;
 
-#pragma warning disable CS8632
 namespace PetShop
 {
     class Program
@@ -12,7 +11,7 @@ namespace PetShop
             Console.WriteLine("Welcome to our petshop!");
 
             // Defining animals and sellables to loop over later.
-            List<Animal?> animals = new()
+            List<Animal> animals = new()
             {
                 new Dog(),
                 new Bird(),
@@ -20,35 +19,78 @@ namespace PetShop
                 // new Blåhaj()
             };
 
+            List<ISellable> sellables = new()
+            {
+                new Toys(),
+                new Food()
+            };
+
+            // Retrieving certain values to be printed later.
+            // Should the scope expand, it'll be a good idea to rework this.
+            double totalPrice = CombinedPriceAnimals(animals) + CombinedPriceSellables(sellables);
+            double sellableWeight = CombinedWeightSellables(sellables);
 
             // All the printing
-            Console.WriteLine("We have the following animals on display:");
-            PrintSpecies(animals);
+            Console.WriteLine("\nWe have the following animals on display:");
+            AnimalPrintSpecies(animals);
 
             Console.WriteLine("\nIf you listen closely, you can even hear them!");
-            PrintSounds(animals);
+            AnimalPrintSounds(animals);
 
             Console.WriteLine("\nWe also sell the following toys and treats:");
+            SellablesPrintNames(sellables);
+
+            Console.WriteLine($"\nIf you'd like to buy one of each, it will cost you exactly {totalPrice} EUR,\n"
+                              + $"and if you'd like to know how much one of each item weights, it's {sellableWeight}kg.");
         }
 
         // Main functions
-        public static void PrintSpecies(List<Animal?> animals)
+        public static void AnimalPrintSpecies(List<Animal> animals)
+        // Prints the species of every animal
         {
-            foreach (Animal animal in animals) {
-                if (animal is not null) Console.WriteLine($"- {animal.GetSpecies()}");
-            }
+            foreach (Animal animal in animals) { Console.WriteLine($"- {animal.GetSpecies()}"); }
         }
 
-        public static void PrintSounds(List<Animal?> animals)
+        public static void AnimalPrintSounds(List<Animal> animals)
         // Prints the sound of every animal
         {
-            foreach (Animal animal in animals) {  if (animal is not null) animal.MakeSound(); }
+            foreach (Animal animal in animals) {   animal.MakeSound(); }
         }
 
-        // Helper functions
-        public static string CurrencyToString(double value)
+        public static double CombinedPriceAnimals(List<Animal> animals)
+        // Prints out the combined price of every animal
         {
-            return value.ToString("C", CultureInfo.CurrentCulture);
+            double totalPrice = 0.0;
+
+            foreach (Animal animal in animals) { totalPrice += animal.Price; }
+
+            return totalPrice;
+        }
+
+        public static void SellablesPrintNames(List<ISellable> sellables)
+        // Prints out the name of every item in stock
+        {
+            foreach (ISellable sellable in sellables) { Console.WriteLine($"- {sellable.GetName()}"); }
+        }
+
+        public static double CombinedPriceSellables(List<ISellable> sellables)
+        // Prints out the combined price of every sellable item
+        {
+            double totalPrice = 0.0;
+
+            foreach (ISellable sellable in sellables) { totalPrice += sellable.GetPrice(); }
+
+            return totalPrice;
+        }
+
+        public static double CombinedWeightSellables(List<ISellable> sellables)
+        // Prints out the combined weight of every sellable
+        {
+            double totalWeight = 0.0;
+
+            foreach (ISellable sellable in sellables) { totalWeight += sellable.GetWeight(); }
+
+            return totalWeight;
         }
     }
 }
---
+++ PetShop/Sellables.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PetShop
+{
+    public interface ISellable
+    {
+        string GetName()
+            {
+                return GetType().Name;
+            }
+        double GetPrice();
+        double GetWeight();
+    }
+
+    public class Toys : ISellable
+    {
+        // This doesn't feel like the right way to do this. Too roundabout? Like, why would you use this over an abstract class?
+        public double GetPrice()
+        {
+            return 5.99;
+        }
+
+        public double GetWeight()
+        {
+            return 0.056;
+        }
+
+    }
+    
+    public class Food : ISellable
+    {
+        public double GetPrice()
+        {
+            return 18.89;
+        }
+
+        public double GetWeight()
+        {
+            return 0.25;
+        }
+    }
+}
